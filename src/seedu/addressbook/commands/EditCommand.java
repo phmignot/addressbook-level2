cd5b626b9087e9e810bfe7793cf2c2bf424fcb6b
PH <e0271089@u.nus.edu>
2018-02-12 12:30:34 +0100

    Constructor and execute methods for EditCommand

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -1,5 +1,6 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.*;
 import seedu.addressbook.data.tag.Tag;
@@ -20,8 +21,35 @@ public class EditCommand extends Command {
             + "Example: " + COMMAND_WORD 
             + "i/1 John Doe ";
 
-    public EditCommand(int targetVisibleIndex,String name) throws IllegalValueException {
+    public static final String MESSAGE_DUPLICATE_PERSON = "You can't change a person into an already existing one in the address book";
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+
+
+
+    private final Person NewPerson;
+    private final ReadOnlyPerson OldPerson;
+
+    public EditCommand(int targetVisibleIndex,String newName) throws IllegalValueException {
         super(targetVisibleIndex);
+        this.OldPerson = getTargetPerson();
+        this.NewPerson = (Person) OldPerson;
+        this.NewPerson.setName(new Name(newName));
+    }
+    
+
+    @Override
+    public CommandResult execute() {
+        try {
+            addressBook.editPerson(OldPerson,NewPerson);
+            return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, OldPerson));
+
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (UniquePersonList.PersonNotFoundException pnfe) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+        } catch (UniquePersonList.DuplicatePersonException dpe) {
+            return new CommandResult(MESSAGE_DUPLICATE_PERSON);
+        }
     }
 }
 
